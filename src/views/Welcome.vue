5a7180ab6d0774cf60a1f803ed65ba6b58f730cd
yeeyeemon@onenex.com <yeeyeemon@onenex.com>
2023-10-26 22:09:42 +0630

    project setup and add global css file

---
+++ src/views/Welcome.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="container welcome">
+        <p>Welcome</p>
+    </div>
+</template>
+<style>
+    .welcome{
+        padding: 20px 0;
+        text-align: center;
+    }
+</style>
