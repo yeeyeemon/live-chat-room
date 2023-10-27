dd8e679748b73bd4f08be504cadb3f7c72c8d536
yeeyeemon@onenex.com <yeeyeemon@onenex.com>
2023-10-27 22:02:25 +0630

    firebase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore'; // Add other Firebase services as needed
+import { isNavigationFailure } from 'vue-router';
+const firebaseConfig = {
+    apiKey: "AIzaSyDqUAP8gX3wqnHjC1QxmaH7XvhcXq11xWs",
+    authDomain: "live-chat-2d5a0.firebaseapp.com",
+    projectId: "live-chat-2d5a0",
+    storageBucket: "live-chat-2d5a0.appspot.com",
+    messagingSenderId: "929874977441",
+    appId: "1:929874977441:web:77d1c0184b8026270406e0",
+    measurementId: "G-HBDCJE42QD"
+  }; 
+
+  firebase.initializeApp(firebaseConfig);
+  let db = firebase.firestore();
+  let timestamp = firebase.firestore.FieldValue.serverTimeStamp;
+
+  export{db, timestamp}
